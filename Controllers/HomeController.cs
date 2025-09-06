6b1962dcd12a55378f90654645624284c18b89a9
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-09-06 14:35:21 +0530

    Implement event listing and enhance layout for StarEvents ticketing system

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EventTicketingSystem.Models;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace EventTicketingSystem.Controllers;
 
@@ -13,14 +15,26 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    // GET: /
     public IActionResult Index()
     {
-        return View();
+        var events = new List<EventCardVm>
+        {
+            new EventCardVm { Title = "Rock Night Colombo", DateTime = "Sun 12 Oct 2025, 7:00 PM", Venue = "Nelum Pokuna", Price = "LKR 5,000", Availability = "120 / 800" },
+            new EventCardVm { Title = "Classical Evening",   DateTime = "Mon 20 Oct 2025, 6:30 PM", Venue = "BMICH",        Price = "LKR 3,500", Availability = "34 / 500" }
+        };
+
+        return View(events);
     }
 
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    public IActionResult About()
+    {
+    return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
