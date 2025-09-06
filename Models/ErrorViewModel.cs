154ac2ee4051571e624dbac8a1d2261c1d398845
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-09-06 13:29:48 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EventTicketingSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
